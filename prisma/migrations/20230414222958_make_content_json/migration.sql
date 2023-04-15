133bcebb6c3d9408050eb3ccdfc82b030faec0c8
KiwiKid <KiwiKid@users.noreply.github.com>
2023-04-16 10:07:22 +1200

    more truncate

--- prisma/migrations/20230414222958_make_content_json/migration.sql
+++ prisma/migrations/20230414222958_make_content_json/migration.sql
@@ -4,6 +4,8 @@
   - Changed the type of `content` on the `Place` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
 
 */
+
+TRUNCATE TABLE "Place";
 -- AlterTable
 ALTER TABLE "Place" DROP COLUMN "content",
 ADD COLUMN     "content" JSONB NOT NULL;
